1eb8358459b14f6c6001822f8f36c31c909c99a4
Jorge Gallegos <kad@blegh.net>
2014-03-19 09:55:17 -0700

    Split these into a separate reqs.txt
    This way I can ``pip install -r`` them previous to test

--- setup.py
+++ setup.py
@@ -4,22 +4,28 @@
 # vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
 #
 
+import os
 import setuptools
 import sys
 
 import table
 
 
+readme = os.path.join(os.path.dirname(sys.argv[0]), 'README.md')
+reqs = os.path.join(os.path.dirname(sys.argv[0]), 'requirements.txt')
+test_reqs = os.path.join(os.path.dirname(sys.argv[0]), 'test-requirements.txt')
+
 install_requires = [
     _.strip()
-    for _ in open('requirements.txt').readlines()
+    for _ in open(reqs).readlines()
     if _ and not _.startswith('#')
 ]
 
 # only required when you are doing testing
 test_requires = [
-    'moto',
-    'pep8',
+    _.strip()
+    for _ in open(test_reqs).readlines()
+    if _ and not _.startswith('#')
 ]
 
 args = sys.argv[1:]
@@ -35,6 +41,13 @@ setuptools.setup(
     version=table.__version__,
     author='Jeremy Grosser',
     author_email='jeremy@synack.me',
+    description='Uses inotify to monitor Cassandra SSTables and upload them to S3',
+    long_description=open(readme).read(),
+    keywords=[
+        'cassandra',
+        'inotify',
+        'backup',
+    ],
     packages=setuptools.find_packages('table'),
     package_dir={
         '': 'table',
