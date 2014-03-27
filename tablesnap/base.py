3e0d466a17a7c527afa0a73935d3fafad79a939d
Jorge Gallegos <kad@blegh.net>
2014-03-27 14:58:04 -0700

    Move some frequently used functions to base
    Some of these I will use in the other scripts and I'm trying to skip
    copy/paste as much as possible

--- tablesnap/base.py
+++ tablesnap/base.py
@@ -4,8 +4,52 @@
 # vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
 #
 
+from __future__ import absolute_import
+
+import boto
 import logging
+import multiprocessing
 import os
+
+
+# Default number of writer threads
+DEFAULT_THREADS = multiprocessing.cpu_count()
+
+
+def get_dir_snapshot(paths, recurse, include=None):
+    """
+    Grabs a directory listing of the given paths and returns a simple list,
+    after filtering out results
+    """
+
+    filenames = []
+    for path in paths:
+        if recurse:
+            for root, dirs, files in os.walk(path):
+                for filename in files:
+                    if include is None or (
+                        include is not None
+                        and include(filename)
+                    ):
+                        filenames.append(filename)
+        else:
+            for filename in os.listdir(path):
+                if include is None or (
+                    include is not None
+                    and include(filename)
+                ):
+                    filenames.append(filename)
+    return filenames
+
+
+def s3_connect(key, secret, debug=0):
+    s3 = boto.connect_s3(key, secret, debug=debug)
+    return s3
+
+
+def get_bucket(connection, bucket_name):
+    # Reconnect to S3
+    return connection.get_bucket(bucket_name)
 
 
 def set_logger(program='tablesnap'):
